9538f88cb7f0e24161abfa578d55764cee82e33e
arafat.habib <arafat.habib@metrobank.com.ph>
2020-08-30 22:57:04 +0800

    create order

--- src/actions/orderActions.js
+++ src/actions/orderActions.js
@@ -1,20 +1,27 @@
-import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER } from "../types";
+import { CREATE_ORDER, CLEAR_CART, CLEAR_ORDER, FETCH_ORDERS } from "../types";
 
 export const createOrder = (order) => (dispatch) => {
-  fetch("/api/orders", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(order),
-  })
-    .then((res) => res.json())//convert data to json
-    .then((data) => {
-      dispatch({ type: CREATE_ORDER, payload: data });
-      localStorage.clear("cartItems");
-      dispatch({ type: CLEAR_CART });
-    });
+    fetch("/api/orders", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(order),
+    })
+        .then((res) => res.json())//convert response to json format
+        .then((data) => {//data contains list of orders
+            dispatch({ type: CREATE_ORDER, payload: data });
+            localStorage.clear("cartItems");
+            dispatch({ type: CLEAR_CART });
+        });
 };
 export const clearOrder = () => (dispatch) => {
-  dispatch({ type: CLEAR_ORDER });
+    dispatch({ type: CLEAR_ORDER });
+};
+export const fetchOrders = () => (dispatch) => {
+    fetch("/api/orders")
+        .then((res) => res.json())//convert response to json format
+        .then((data) => {//data contains list of orders
+            dispatch({ type: FETCH_ORDERS, payload: data });
+        });
 };
